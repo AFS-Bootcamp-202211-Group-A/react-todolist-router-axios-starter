989e281f15e30a0c2cef597eedfafd351acbb9e3
MICHAEL NAM (DSENG-ISD-OOCLL/HKG) <NAMMI@OOCL.COM>
2022-12-14 15:01:51 +0800

    feat: use axios to get todos and show list

---
+++ src/api/todos.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+const api = axios.create({
+    baseURL: "https://63996b3529930e2bb3d236a5.mockapi.io"
+});
+
+export const getTodos = () => {
+    return api.get("/todos")
+};
