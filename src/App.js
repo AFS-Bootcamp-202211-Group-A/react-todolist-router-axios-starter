040dbc1e2344cdc7f87869eba1d384a867beeabd
Marie Chow <chowma3@oocl.com>
2022-12-14 14:25:22 +0800

    feat: add Layout for nav

--- src/App.js
+++ src/App.js
@@ -4,15 +4,18 @@ import TodoList from "./features/todo/TodoList";
 import AboutPage from "./pages/AboutPage";
 import DoneTodo from "./features/todo/DoneTodo";
 import NotFoundPage from "./pages/NotFoundPage";
+import Layout from "./layout/Layout";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<TodoList />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/done" element={<DoneTodo />} />
-        <Route path="*" element={<NotFoundPage />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<TodoList />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/done" element={<DoneTodo />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Route>
       </Routes>
     </div>
   );
