8f7f0ef4e7b32846d632aeaa08cf74e5a0ee479b
HoiDam <66233626+HoiDam@users.noreply.github.com>
2022-12-14 23:26:32 +0800

    feat: updated 404 page

--- src/pages/NotFoundPage.js
+++ src/pages/NotFoundPage.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import { Image } from 'antd';
 
 export default function NotFoundPage() {
   return (
-    <div>NotFoundPage</div>
+    <div>
+      <Image src='https://cdn.discordapp.com/attachments/506057846991880240/1052607201891602472/image.png'>
+
+      </Image>
+    </div>
   )
 }
