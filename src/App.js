c517e53a10dfe598fdd039901d8c65a5c99c3abb
kwokth4 <kwokth4@oocl.com>
2022-12-14 11:04:50 +0800

    feat: Route with different pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import "./App.css";
 import TodoList from "./features/todo/TodoList";
+import {Routes, Route} from "react-router-dom";
+import AboutPage from "./pages/AboutPage";
+import NotFoundPage from "./pages/NotFoundPage";
+import DonePage from "./pages/DonePage";
 
 function App() {
   return (
     <div className="App">
-      <TodoList />
+      {/* <TodoList /> */}
+      <Routes>
+        <Route path="/" element={<TodoList />} />;
+        <Route path="/about" element={<AboutPage/>} />;
+        <Route path="*" element={<NotFoundPage/>} />;
+        <Route path="/done" element={<DonePage/>} />;
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/DonePage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function DonePage() {
+  return (
+    <div>DonePage</div>
+  )
+}
+
+export default DonePage
