b991c704983757409130892ab216699b6093d2c6
HoiDam <66233626+HoiDam@users.noreply.github.com>
2022-12-14 15:03:06 +0800

    feat: use axios to get

---
+++ src/api/todos.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const api = axios.create({
+    baseURL: "https://63996b3629930e2bb3d236e0.mockapi.io"
+});
+
+export const getTodos = ()=>{
+    
+    return api.get("/todos")
+}
+
