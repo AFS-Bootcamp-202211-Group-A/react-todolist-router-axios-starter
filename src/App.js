b341beb3b079d391ce10e759d0b75d12267b749b
heinrich-oocl <117629815+heinrich-oocl@users.noreply.github.com>
2022-12-14 11:06:30 +0800

    feat: added router to architecture

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import AboutPage from "./app/pages/AboutPage";
+import NotFoundPage from "./app/pages/NotFoundPage";
+import TodoPage from "./app/pages/TodoPage";
 import TodoList from "./features/todo/TodoList";
 
 function App() {
-  return (
-    <div className="App">
-      <TodoList />
-    </div>
-  );
+    return (
+        <div className="App">
+            <Routes>
+              <Route path="/" element={<TodoPage/>}/>
+              <Route path="/todolist" element={<TodoPage/>}/>
+              <Route path="/about" element={<AboutPage/>}/>
+              <Route path="/*" element={<NotFoundPage/>}/>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
