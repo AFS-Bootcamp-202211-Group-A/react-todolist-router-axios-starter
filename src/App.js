f265e380a7166f4a7b493871d92614bd6d6fbd70
MICHAEL NAM (DSENG-ISD-OOCLL/HKG) <NAMMI@OOCL.COM>
2022-12-14 10:57:35 +0800

    add pages for routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import TodoList from "./features/todo/TodoList";
+import AboutPage from "./pages/AboutPage";
+import DoneListPage from "./pages/DoneListPage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 function App() {
   return (
     <div className="App">
-      <TodoList />
+      {/* <TodoList /> */}
+      <Routes>
+        <Route path="/" element={<TodoList />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/done" element={<DoneListPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
     </div>
   );
 }
