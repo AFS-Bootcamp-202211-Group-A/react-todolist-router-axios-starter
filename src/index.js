7d792da2f9ce671e06babc20fd4197a6d04be6b5
kwokth4 <kwokth4@oocl.com>
2022-12-14 16:45:31 +0800

    installed antd

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
 import { store } from "./app/store";
 import App from "./App";
+import "antd/dist/reset.css";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
