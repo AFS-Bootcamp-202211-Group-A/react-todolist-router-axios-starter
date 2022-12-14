0e934d7ef82e923ef54bf2c21b19a6ec25311a73
Marie Chow <chowma3@oocl.com>
2022-12-14 20:22:57 +0800

    fix: render doneList using api

--- src/features/todo/DoneTodo.js
+++ src/features/todo/DoneTodo.js
@@ -1,8 +1,19 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getTodos } from "../../api/todos";
 import TodoGroup from "./TodoGroup";
+import { addTodos } from "./todoSlice";
 
 export default function DoneTodo() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    getTodos()
+      .then((response) => {
+        dispatch(addTodos(response.data));
+      })
+      .catch((error) => console.log(error));
+  }, [dispatch]);
+
   const dones = useSelector((state) => {
     return state.todoList.filter((todo) => todo.done);
   });
