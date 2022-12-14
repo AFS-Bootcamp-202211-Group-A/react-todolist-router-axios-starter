b3a3338a56bb74bad968e04f5faa0efde555b94f
Marie Chow <chowma3@oocl.com>
2022-12-14 15:00:22 +0800

    feat: fetch todos from api and add to store

---
+++ src/api/todos.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "https://63996b3316b0fdad773c180e.mockapi.io/",
+});
+
+export const getTodos = () => {
+  return api.get("/todos");
+};
