35bbda6c02dcd2dad30d8fbe4c022200f61843ae
Marie Chow <chowma3@oocl.com>
2022-12-14 21:09:26 +0800

    feat: use Menu instead of nav

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,14 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, Outlet } from "react-router-dom";
+import { AppstoreOutlined, CheckSquareOutlined } from "@ant-design/icons";
+import { Menu } from "antd";
 
 export default function Layout() {
+  const items = [
+    {
+      label: <Link to="/">Home</Link>,
+      key: "home",
+      icon: <AppstoreOutlined />,
+    },
+    {
+      label: <Link to="/about">About</Link>,
+      key: "about",
+      icon: <AppstoreOutlined />,
+    },
+    {
+      label: <Link to="/done">Done</Link>,
+      key: "done",
+      icon: <CheckSquareOutlined />,
+    },
+  ];
+  const [current, setCurrent] = useState("home");
+  const onClick = (event) => {
+    setCurrent(event.key);
+  };
   return (
     <div>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/done">Done</Link>
-      </nav>
+      <Menu
+        onClick={onClick}
+        selectedKeys={[current]}
+        mode="horizontal"
+        items={items}
+      />
       <div className="content">
         <Outlet />
       </div>
