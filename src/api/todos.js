fb5d2febdc250954f4b7c9bb8ad47455c278f64c
kwokth4 <kwokth4@oocl.com>
2022-12-15 14:23:31 +0800

    feat: connect mongodb

--- src/api/todos.js
+++ src/api/todos.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: "https://63996b3829930e2bb3d23717.mockapi.io",
+    // baseURL: "https://63996b3829930e2bb3d23717.mockapi.io",
+    baseURL: "http://localhost:8080",
 })
 
 export const getTodos = () => {
