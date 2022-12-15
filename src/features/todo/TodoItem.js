1586d9d0e044afcc0fa47c5090e92d11f774f88d
MICHAEL NAM (DSENG-ISD-OOCLL/HKG) <NAMMI@OOCL.COM>
2022-12-15 13:50:48 +0800

    feat: delete from backend

--- src/features/todo/TodoItem.js
+++ src/features/todo/TodoItem.js
@@ -6,6 +6,8 @@ import { apiDeleteTodo, putTodo } from "../../api/todos";
 import "./TodoItem.css";
 import TodoModal from "./TodoModal";
 
+const NO_CONTENT = 204;
+
 const TodoItem = (props) => {
   const { todo } = props;
   const dispatch = useDispatch();
@@ -20,22 +22,19 @@ const TodoItem = (props) => {
 
   const onDelete = (event) => {
     event.stopPropagation();
-    apiDeleteTodo(todo.id).then((response) =>
-      dispatch(deleteTodo(response.data.id))
-    );
+    apiDeleteTodo(todo.id).then((response) => {
+      if (response.status === NO_CONTENT) dispatch(deleteTodo(todo.id));
+    });
   };
 
   return (
     <div className="box">
       <Row justify="end">
         <Col flex="auto" onClick={onToggle}>
-          <span  className={todo.done ? "done" : ""}>
-            {todo.text}
-          </span>
+          <span className={todo.done ? "done" : ""}>{todo.text}</span>
         </Col>
         <Col>
-          <TodoModal todo={todo} />
-          {" "}
+          <TodoModal todo={todo} />{" "}
           <Button
             onClick={onDelete}
             size={"small"}
